31187a33d87ee09b81e4f4e1bbee7580f179ec66
mohamedezz01 <138720737+mohamedezz01@users.noreply.github.com>
2025-02-19 17:29:40 +0200

    Updated railway_v2

---
+++ src/main/java/com/example/crud/security/MessageConfig.java
@@ -0,0 +1,22 @@
+package com.example.crud.security;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessageConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+
+        messageSource.setCacheSeconds(7200);
+
+        return messageSource;
+    }
+}
