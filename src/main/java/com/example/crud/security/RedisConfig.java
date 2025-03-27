675442260288a728e58ebd63d82cb5885c306081
mohamedezz01 <138720737+mohamedezz01@users.noreply.github.com>
2025-03-28 00:35:30 +0200

    Updated railway_v6,with redis and lombok update

--- src/main/java/com/example/crud/security/RedisConfig.java
+++ src/main/java/com/example/crud/security/RedisConfig.java
@@ -1,14 +1,34 @@
 package com.example.crud.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        System.out.println("Connecting to Redis: " + redisHost + ":" + redisPort);
+        System.out.println(" Redis Password: " + (redisPassword.isEmpty()));
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+        factory.setPassword(redisPassword);
+        return factory;
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
