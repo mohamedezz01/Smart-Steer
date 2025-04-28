b935d11d6b5a45147357a9c23782c64fac5bbb47
mohamedezz01 <138720737+mohamedezz01@users.noreply.github.com>
2025-04-28 17:22:42 +0300

    Updated v7

--- src/main/java/com/example/crud/service/NotificationService.java
+++ src/main/java/com/example/crud/service/NotificationService.java
@@ -9,18 +9,7 @@ import java.util.Map;
 @Service
 public class NotificationService {
 
-
-    public void sendEmergencyNotification(String fcmToken, Map<String, String> data) throws FirebaseMessagingException {
-        Message message = Message.builder()
-                .setToken(fcmToken)
-                .putAllData(data)
-                .build();
-
-        String response = FirebaseMessaging.getInstance().send(message);
-        System.out.println("Notification sent. Response: " + response);
-    }
-
-    public void sendNotification(String fcmToken, String title, String body) throws FirebaseMessagingException {
+    public void sendEmergencyNotification(String fcmToken, String title, String body) throws FirebaseMessagingException {
         Map<String, String> data = new HashMap<>();
         data.put("type", "accident");
 
