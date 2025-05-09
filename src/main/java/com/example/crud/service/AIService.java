e2994bef8bbd58ee969eb0d03aa5a9b949440773
mohamedezz01 <138720737+mohamedezz01@users.noreply.github.com>
2025-05-10 01:10:28 +0300

    Updated v7

---
+++ src/main/java/com/example/crud/service/AIService.java
@@ -0,0 +1,13 @@
+package com.example.crud.service;
+
+import org.springframework.stereotype.Service;
+
+import java.awt.image.BufferedImage;
+
+@Service
+public class AIService {
+    public String processImage(BufferedImage image) {
+
+        return "stop"; // Example response
+    }
+}
