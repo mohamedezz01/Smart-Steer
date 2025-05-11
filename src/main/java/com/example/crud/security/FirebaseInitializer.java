2f4a9f33e663fc629a162d5226dc2803b8fa8428
mohamedezz01 <138720737+mohamedezz01@users.noreply.github.com>
2025-05-11 03:20:00 +0300

    Updated ultrasonic reading left,right

--- src/main/java/com/example/crud/security/FirebaseInitializer.java
+++ src/main/java/com/example/crud/security/FirebaseInitializer.java
@@ -4,44 +4,32 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 @Component
 public class FirebaseInitializer {
 
-    @Value("${firebase.config}")
-    private String firebaseConfigJson;
-
     @PostConstruct
     public void initialize() {
         try {
-            System.out.println("FirebaseInitializer: Attempting to load firebase.config secret."); // New log
-            String firebaseConfig = this.firebaseConfigJson;
-            if (firebaseConfig != null) {
-                firebaseConfig = firebaseConfig.replace("\\n", "\n");
+            InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
 
-                InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
-                FirebaseOptions options = new FirebaseOptions.Builder()
-                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                        .build();
-
-                if (FirebaseApp.getApps().isEmpty()) {
-                    FirebaseApp.initializeApp(options);
-                    System.out.println("Firebase initialized.");
-                } else {
-                    System.out.println("Firebase already initialized.");
-                }
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                System.out.println("Firebase initialized.");
             } else {
-                System.out.println("FIREBASE_CONFIG not found. Firebase not initialized.");
+                System.out.println("Firebase already initialized.");
             }
 
         } catch (Exception e) {
-            System.out.println(" Failed to initialize Firebase: " + e.getMessage());
+            System.out.println("Failed to initialize Firebase: " + e.getMessage());
         }
     }
 }
